62bebd894c9890a4142910c25a322bb1943aef79
Ian Lee <ianlee@airbears2-10-142-48-252.airbears2.1918.berkeley.edu>
2016-02-19 16:40:29 -0800

    done

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,3 +1,9 @@
 class Foobar
-  # ENTER CODE FOR Q2 HERE
+    def initialize(a)
+        @var = a
+    end
+
+    def bar(b, options={})
+        b.to_s +  @var + options[:sat].to_s
+    end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,28 @@
 class PagesController < ApplicationController
   def home
-    # foo = Foobar.new "baz"
-    # @baz = foo.bar :cat, sat: :dat, dat: :sat
+    foo = Foobar.new "baz"
+    @baz = foo.bar :cat, sat: :dat, dat: :sat
   end
 
   def stringify
-    @text = "You are nothing!"
+    @name = params[:name]
+    @adjective = params[:adjective]
+    if @adjective.blank?
+      @text = "You are nothing!"
+    else
+      @text = @name + " is so " + @adjective
+    end
   end
 
   def age
   end
 
   def person
+    @name = params[:name]
+    @age = params[:age]
+    you = Person.new(@name, @age)
+    @introduction = you.introduce() 
+    @birth_year = you.birth_year().to_s
+    @nickname = you.nickname()
   end
 end
